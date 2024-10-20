bce5e2264c373c9170c1b4bd3f7105e1fd37a22b
Abdul Moiz <158452717+moiz2723@users.noreply.github.com>
2024-10-21 00:39:43 +0500

    Add files via upload
    Lab Task

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int userAge;
+
+  string userName;
+  cout<<"enter your name";
+  cin>> userName;
+
+  cout<<"enter your age";
+ cin>> userAge;
+
+  cout<< "Welcome" << userName <<endl;
+ 
+  cout<<"have a great day dear!\n";
+
+  
+  return 0;
+}
