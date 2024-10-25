5dfd7e89215c5e22a7a48802cfe137d6f5393d49
Abdul Moiz <158452717+moiz2723@users.noreply.github.com>
2024-10-26 01:52:09 +0500

    Add files via upload
    In this float values will converted to integer values

---
+++ floatToint.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+float a;
+cout<<"Enter any float value:"<<endl;
+cin>>a;
+
+int b;
+cout<<"enter any integer value:"<<endl;
+cin>>b;
+ 
+a=b;
+
+cout<<b<< "is your integer value"<<endl;
+
+
+
+
+
+
+    return 0;
+}
