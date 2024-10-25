8a2c0d31f8752dfc1d49d4c9dc40babf714ee35c
Abdul Moiz <158452717+moiz2723@users.noreply.github.com>
2024-10-26 01:28:07 +0500

    Update initialization.cpp

--- initialization.cpp
+++ initialization.cpp
@@ -7,13 +7,13 @@ int main(){
  char b='b';
  bool c=true;
  float d=1.1;
- string name="moiz";
+
 
  cout<<a<<endl; 
 cout<<b<<endl;
  cout<<c<<endl; 
  cout<<d<<endl; 
-cout<<name<<endl;
+
 
     return 0;
 }
