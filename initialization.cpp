8359fd25564e50b97230659aabd07d2c93c51bc0
Abdul Moiz <158452717+moiz2723@users.noreply.github.com>
2024-10-26 01:27:18 +0500

    Add files via upload
    I this i initialize values and simple print out ..

---
+++ initialization.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    
+ int a=5;
+ char b='b';
+ bool c=true;
+ float d=1.1;
+ string name="moiz";
+
+ cout<<a<<endl; 
+cout<<b<<endl;
+ cout<<c<<endl; 
+ cout<<d<<endl; 
+cout<<name<<endl;
+
+    return 0;
+}
