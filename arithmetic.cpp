11af9738757835b57bec98c5b43c793638f335d7
Abdul Moiz <158452717+moiz2723@users.noreply.github.com>
2024-10-26 23:00:01 +0500

    Add files via upload
    In this program i perform arithmetic operations on integers and float values.

---
+++ arithmetic.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int num1 = 10;
+ int num2= 5;
+
+ float num3 = 10.5;
+ float  num4 = 20.2;
+
+ cout<<"integers opertaions: "<<endl;
+ cout<<" Addition: "<<num1 + num2<<endl;
+cout<<" Multiplication: "<<num1 * num2<<endl;
+cout<<" Subtraction: "<<num1 - num2<<endl;
+cout<<" division: "<<num1 / num2<<endl;
+
+cout<<" Float Opreations: "<<endl;
+cout<<" addition: "<<num3 + num4 <<endl;
+cout<<" Subtraction: "<<num3 - num4 <<endl;
+cout<<" Multiplication: "<<num3 * num4 <<endl;
+cout<<" Division: "<<num3 / num4 <<endl;
+
+    return 0;
+}
